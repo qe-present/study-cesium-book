454e21a47257aaa40d2755f2103b49e9e8d63163
qe-present <2664481691@qq.com>
2025-03-04 18:12:37 +0800

    add

---
+++ 01-quick-start/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted } from 'vue';
+import {TOKEN} from '@/constant/data.js'
+import * as Cesium from 'cesium'
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken = TOKEN
+  const viewer = new Cesium.Viewer('map', {
+    geocoder:true,  // 是否显示位置查找工具
+    homeButton:true, // 是否显示首页位置工具
+    sceneModePicker:true, // 是否显示视角模式切换工具
+    baseLayerPicker:true, // 是否显示默认图层选择工具
+    navigationHelpButton:true, // 是否显示导航帮助工具
+    animation:true, // 是否显示动画工具
+    timeline:true, // 是否显示时间轴工具
+    fullscreenButton:true, // 是否显示全屏按钮工具
+    infoBox:false, // 是否显示信息框
+  })
+})
+</script>
+
+<template>
+<div id="map"></div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#map {
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ 01-quick-start/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import cesium from 'vite-plugin-cesium'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(),cesium()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
